8fee78b77f09b8b77a4f46be63ce7a981cca0631
Nafalaly <nafalaly@gmail.com>
2024-11-30 20:14:35 +0700

    Convert jemaat to res.partner

--- custom_addons/models/jemaat.py
+++ custom_addons/models/jemaat.py
@@ -1,21 +1,47 @@
+#  -*- coding: utf-8 -*-
 from odoo import models, fields, api
 from odoo.exceptions import UserError
 from odoo.tools.translate import _
 
 
 class Jemaat(models.Model):
-    _name = 'jemaat'
-    _inherit = ['mail.thread', 'mail.activity.mixin']
-    _rec_name = 'name'
+    """
+    Inherit res.partner
+    to full fill Jemaat base information's
+    """
+    _inherit = 'res.partner'
 
-    nomor = fields.Char('Nomor Jemaat', required=True, index=True, readonly=True, default=lambda self: _('New'))
-    name = fields.Char('Nama Jemaat', required=True)
-    tempat_lahir = fields.Char(string='Tempat Lahir', required=True)
+    is_jemaat = fields.Boolean(string='Jemaat', help='True if Partner is Jemaat')
+    jemaat_number = fields.Char('Nomor Jemaat', required=True, index=True, readonly=True,
+                                default=lambda self: _('New'), tracking=True)
     tanggal_lahir = fields.Date(string='Tanggal Lahir', required=True)
+    tempat_lahir = fields.Char(string='Tempat Lahir', required=True)
     jenis_kelamin = fields.Selection([
-        ('laki-laki', 'Laki-Laki'),
-        ('wanita', 'Wanita')
-    ], 'Jenis Kelamin')
+        ('male', 'Laki-Laki'),
+        ('female', 'Wanita')
+    ], 'Jenis Kelamin', required=True)
+
+    @api.onchange('pekerjaan')
+    def onchange_pekerjaan(self):
+        """
+        Method to copy value from pekerjaan to default 'function' field in
+        res.partner
+        """
+        for rec in self:
+            if rec.pekerjaan not in ['tidak bekerja', 'lain-lain']:
+                rec.function = rec.pekerjaan
+    # TODO: Create Umur computed (?)
+
+    pekerjaan = fields.Selection([
+        ('tidak bekerja', 'Tidak Bekerja'),
+        ('pegawai negeri', 'Pegawai Negeri'),
+        ('pegawai swasta', 'Pegawai Swasta'),
+        ('wiraswasta', 'Wiraswasta'),
+        ('profesional', 'Profesional'),
+        ('abri', 'ABRI'),
+        ('hamba tuhan', 'Hamba Tuhan'),
+        ('lain-lain', 'Lain-lain'),
+    ], 'Pekerjaan', default="tidak bekerja")
     pendidikan = fields.Selection([
         ('tidak sekolah', 'Tidak Sekolah'),
         ('sekolah dasar', 'SD'),
@@ -23,96 +49,72 @@ class Jemaat(models.Model):
         ('sekolah menengah utama/sekolah menengah kejuruan', 'SLTA/SMU/Sederajat'),
         ('tingkat akademi', 'Tingkat Akademi'),
         ('tingkat universitas', 'Tingkat Universitas/Sarjana'),
-    ], 'Pendidikan Terakhir', default="tidak sekolah")
-    nomor_telpon = fields.Char('Nomor Telpon Rumah/Kantor')
-    nomor_hp = fields.Char('Nomor Handphone')
+    ], 'Pendidikan Terakhir', default="tidak sekolah", tracking=True)
     golongan_darah = fields.Selection([
         ('O', 'O'),
         ('A', 'A'),
         ('B', 'B'),
         ('AB', 'AB'),
-    ], default='', string='Golongan Darah')
-    gereja_id = fields.Many2one(comodel_name='gereja', string='Gereja', ondelete='set null')
-    email = fields.Char(string='Email')
-
+    ], string='Golongan Darah')
+    gereja_id = fields.Many2one(comodel_name='gereja', string='Gereja', ondelete='restrict', index=True, required=True,
+                                tracking=True)
     is_baptis = fields.Boolean(string='Sudah dibaptis', default=False)
     is_baptis_roh_kudus = fields.Boolean(string='Sudah dibaptis Roh Kudus', default=False)
-    is_ayah_jemaat = fields.Boolean(string='Apakah Ayah Jemaat?', default=False)
-    nama_ayah = fields.Char(string='Nama Ayah')
-    nama_ayah_jemaat_id = fields.Many2one(comodel_name='jemaat', string='Nama Ayah')
-    is_ibu_jemaat = fields.Boolean(string='Apakah Ibu Jemaat?', default=False)
-    nama_ibu = fields.Char(string='Nama Ibu')
-    nama_ibu_jemaat_id = fields.Many2one(comodel_name='jemaat', string='Nama Ibu')
+    is_ayah_jemaat = fields.Boolean(string='Apakah Ayah Jemaat?', related='ayah_jemaat_id.is_jemaat')
+    ayah_jemaat_id = fields.Many2one(comodel_name='res.partner', string='Ayah', tracking=True)
+    ibu_jemaat_id = fields.Many2one(comodel_name='res.partner', string='Ibu', tracking=True)
+    is_ibu_jemaat = fields.Boolean(string='Apakah Ibu Jemaat?', related='ibu_jemaat_id.is_jemaat')
 
     baptisan_id = fields.Many2one(comodel_name='baptisan', string='Nomor Baptisan', ondelete='set null', default=False)
     kkj_id = fields.Many2one(comodel_name='kartu.keluarga.jemaat', string='Nomor KKJ', ondelete='set null',
                              default=False)
 
-    alamat = fields.Text('Alamat')
-    provinsi = fields.Char('Provinsi')
-    kota = fields.Char('Kabupaten/Kota')
-    kecamatan = fields.Char('Kecamatan')
-    kelurahan = fields.Char('Kelurahan/Desa')
-    kodepos = fields.Char('Kodepos')
-
     pengerja_line_id = fields.One2many(comodel_name='pengerja', inverse_name='partner_id', string='Nama Jemaat')
+    pengerja_count = fields.Float(compute='_compute_pengerja_count')
     cool_line_id = fields.One2many(comodel_name='cool.anggota', inverse_name='jemaat_id', string='Nama Jemaat')
     cool_doa_line_id = fields.One2many(comodel_name='cool.kubu.doa', inverse_name='jemaat_id', string='Nama Jemaat')
 
-    # rank_name = fields.Char(string='Rank Name')
-    image_1920 = fields.Image("Foto Jemaat", max_width=1920, max_height=1920)
-    state = fields.Selection([
-        ('draft', 'Draft'),
-        ('approved', 'Approved'),
-        ('cancel', 'Cancel'),
-    ], 'Status', default="draft")
-
     _sql_constraints = [
-        ('nomor_uniq', 'unique (nomor)', "nomor already exists !"),
+        ('nomor_uniq', 'unique (jemaat_number)', "Jemaat Number already exists !"),
     ]
+
+    def _compute_pengerja_count(self):
+        self.pengerja_count = len(self.pengerja_line_id)
 
     @api.model_create_multi
     def create(self, vals_list):
         for vals in vals_list:
             if 'company_id' in vals:
                 self = self.with_company(vals['company_id'])
-            if vals.get('nomor', _("New")) == _("New"):
+            if vals.get('jemaat_number', _("New")) == _("New"):
                 seq_date = fields.Datetime.context_timestamp(
                     self, fields.Datetime.to_datetime(vals['create_date'])
                 ) if 'create_date' in vals else None
-                vals['nomor'] = self.env['ir.sequence'].next_by_code(
+                vals['jemaat_number'] = self.env['ir.sequence'].next_by_code(
                     'app.jemaat.seq', sequence_date=seq_date) or _("New")
 
         return super().create(vals_list)
 
-    @api.ondelete(at_uninstall=False)
-    def _unlink_except_draft_or_cancel(self):
-        for data in self:
-            if data.state not in ('draft', 'cancel'):
-                raise UserError(_(
-                    "You can not delete a data."
-                    " You must first cancel it."))
-
-    def action_cancel(self):
-        self.change_state('cancel')
+    def _prepare_pengerja_vals(self):
+        return {
+            'partner_id': self.id,
+        }
 
     @api.model
-    def is_allowed_transition(self, old_state, new_state):
-        allowed = [('draft', 'approved'),
-                   ('approved', 'draft'),
-                   ('approved', 'cancel')]
-        return (old_state, new_state) in allowed
+    def _action_create_pengerja(self):
+        self.ensure_one()
+        vals = self._prepare_pengerja_vals()
+        return self.env['pengerja'].create(vals)
 
-    def change_state(self, new_state):
-        for data in self:
-            if data.is_allowed_transition(data.state, new_state):
-                data.state = new_state
-            else:
-                msg = _('Moving from %s to %s is not allowed') % (data.state, new_state)
-                raise UserError(msg)
-
-    def approve_jemaat(self):
-        self.change_state('approved')
-
-    def redraft_jemaat(self):
-        self.change_state('draft')
+    def action_create_pengerja(self):
+        if not self.pengerja_line_id:
+            pengerja_id = self._action_create_pengerja()
+            action = self.env["ir.actions.actions"]._for_xml_id("custom_addons.pengerja_action")
+            action.update({
+                'views': [[self.env.ref('custom_addons.pengerja_view_form').id, 'form']],
+                'view_mode': 'form',
+                'target': 'current',
+                'res_id': pengerja_id.id,
+            })
+            return action
+        return False
